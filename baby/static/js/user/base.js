c8aaa5ea15949348acc7e045630962fd7aa97801
yuzhe0918 <xiangyuzhe0918@126.com>
2013-11-19 20:37:26 +0800

    base.js5

--- baby/static/js/user/base.js
+++ baby/static/js/user/base.js
@@ -385,7 +385,7 @@ MZ.app = {
 					setTimeout(function() {
 						var doctorList = json.doctor_list
 						// 调用java方法
-						window.app.webviewLogin(1, 1)
+						window.app.webviewLogin(json.doctor_list.id, json.doctor_list.remember)
 					}, 2000)
 				}
 			})
