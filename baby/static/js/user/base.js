9e0191e14d383970a6bcf504107d6f3a9e6d5a6b
yuzhe0918 <xiangyuzhe0918@126.com>
2013-11-19 18:11:02 +0800

    base.js4

--- baby/static/js/user/base.js
+++ baby/static/js/user/base.js
@@ -385,7 +385,7 @@ MZ.app = {
 					setTimeout(function() {
 						var doctorList = json.doctor_list
 						// 调用java方法
-						window.app.webviewLogin(doctorList.id, doctorList.remember)
+						window.app.webviewLogin(1, 1)
 					}, 2000)
 				}
 			})
