2aee088c38e61d36c4ccbdf98c4da797c364d4b5
yuzhe0918 <xiangyuzhe0918@126.com>
2013-11-25 18:04:43 +0800

    蛋疼呀。

--- baby/static/js/base.js
+++ baby/static/js/base.js
@@ -391,7 +391,7 @@ MZ.app = {
 					setTimeout(function() {
 						var doctorList = json.doctor_list
 						// 调用java方法
-						window.app.webviewLogin(doctorList[0].id)
+						window.app.webviewLogin(doctorList.id)
 					}, 2000)
 				} else {
 					window.Notification.simple(MZ.constant.ACCOUNT_PASSWORD_ERROR, 2000)
