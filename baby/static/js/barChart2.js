8acac2a62a385230d081165ef870ef3e3c1aaf4d
gracer-lyl <892455559@qq.com>
2013-12-16 23:46:52 +0800

    fixed grow_rate.html chart reverl

--- baby/static/js/barChart2.js
+++ baby/static/js/barChart2.js
@@ -1,93 +1,83 @@
 $(function() {
-	var chart = new iChart.ColumnStacked2D({
+	var yAxis = growObj.xAxis
+	var values = growObj.data
+	var i
+	var len = yAxis.length
+	var data = []
+	for (i = 0; i < len; i++) {
+		data.push({
+			name: yAxis[i],
+			value: values[i],
+			color: '#08C'
+		})
+	}
+	var chart = new iChart.Bar2D({
 		render: 'canvasDiv',
-		data: growObj.data,
-		labels: growObj.xAxis,
-		title: {
-			text: '生长速率图',
-			color: '#444',
-			textAlign: 'center',
-			padding: '0 20',
-			font: '微软雅黑',
-			fontsize: 14,
-			height: 30
-		},
-		padding: '2 0',
+		data: data,
 		width: $('.chart-frame').width(),
 		height: 300,
-		border: 0,
-		column_width: 20,
-		background_color: '#FFF',
-		label: {
-			color: '#000',
-			font: '微软雅黑',
-			fontsize: 11,
-			fontweight: 500
-		},
+		offsetx: 5,
 		turn_off_touchmove: true,
-		legend: {
-			enable: true,
-			align: 'center',
-			valign: 'top',
-			column: 2,
-			background_color: null,
-			color: '#444',
-			fontsize: 10,
-			font: '微软雅黑',
-			fontweight: 400,
-			border: {
-				enable: false
-			}
-		},
-		sub_option: {
-			label: {
-				fontsize: 10,
-				fontweight: 400,
-				color: '#FFF'
-			}
-		},
-		column_width: 30,
 		coordinate: {
-			grid_color: '#EEE',
-			background_color: null,
+			width: '65%',
+			height: '80%',
+			grid_color: '#CCC',
 			axis: {
-				color: '#EEE',
-				width: 1
+				color: '#CCC',
+				width: [1, 1, 0, 1]
 			},
 			scale: [{
-				position: 'left',
-				scale_enable: false,
+				position: 'top',
 				start_scale: growObj.yStart,
-				scale_space: growObj.ySpace,
 				end_scale: growObj.yEnd,
+				scale_space: growObj.ySpace,
 				label: {
-					color: '#000',
-					fontsize: 10,
-					fontweight: 400
+					color: '#444'
+				},
+				listeners: {
+					parseText: function(t, x, y) {
+						return {
+							text: t
+						}
+					}
 				}
-			}],
-			width: '80%',
-			height: '80%'
+			}]
+		},
+		label: {
+			color: '#444'
+		},
+		background_color: '#FFF',
+		sub_option: {
+			listeners: {
+				parseText: function(r, t) {
+					return t;
+				}
+			}
+		},
+		legend: {
+			enable: false
 		}
 	});
-
 	//利用自定义组件构造左上侧单位
 	chart.plugin(new iChart.Custom({
 		drawFn: function() {
 			//计算位置
 			var coo = chart.getCoordinate(),
 				x = coo.get('originx'),
-				y = coo.get('originy');
+				y = coo.get('originy'),
+				w = coo.width,
+				h = coo.height;
 			//在左上侧的位置，渲染一个单位的文字
 			chart.target.textAlign('end')
 				.textBaseline('bottom')
-				.textFont('500 11px 微软雅黑')
-				.fillText('平均生长速率', x + 50, y - 20, false, '#000')
+				.textFont('500 11px')
+				.fillText('随访日期', x - 5, y, false, '#444')
+				.textBaseline('top')
+				.fillText('平均生长速率g/day', x + w + 24, y - 32, false, '#444')
+
 		}
 	}));
-
 	chart.draw();
-
 	$(window).resize(function() {
 		chart.resize($('.chart-frame').width(), 300);
 	});
