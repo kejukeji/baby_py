30cae48399bceb2a79a85a38320602bd78200379
kejukeji <github@kejukeji.com>
2014-03-05 14:20:21 +0800

    image_config

--- baby/setting/secret.py
+++ baby/setting/secret.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-EX_DB_USER = ''
-EX_DB_PASSWORD = ''
-EX_DB_NAME = ''
-EX_SECRET_KEY_SERVER = ''
+EX_DB_USER = 'root'
+EX_DB_PASSWORD = 'root'
+EX_DB_NAME = 'doctor_baby'
+EX_SECRET_KEY_SERVER = 'ghjsadhlsadf21312hjlk123hjk123hj'
